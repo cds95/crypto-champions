33d17b6fb0d764cb3f3c864677e9fd41ab61169f
cds95 <cds95@uw.edu>
2021-03-19 23:28:51 -0700

    Implement landing page skeleton

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { setMaxElderSpiritsAction } from '../../redux/actions';
+import { Button } from '@material-ui/core';
+import './LandingPage.css';
+import landingPageImg from '../../test.png'; // Temp for now.  Move this to S3 or something later.
+import { Link } from 'react-router-dom';
+import { routeDefinitions } from '../../routeDefinitions';
 
-export const LandingPageComp = ({ maxElderSpirits, maxNumHeroes }) => {
+export const LandingPageComp = () => {
     return (
-        <div>
-            {maxElderSpirits} {maxNumHeroes}
+        <div className="landing-page">
+            <div className="landing-page__content">
+                <div className="landing-page__header">
+                    <img src={landingPageImg} className="landing-page__img"></img>
+                    <div className="landing-page__caption">sdfsdfs</div>
+                </div>
+                <Link
+                    to={routeDefinitions.CREATION_WORKFLOW}
+                    component={(props) => (
+                        <Button className="landing-page__get-started" variant="contained" color="primary" {...props}>
+                            Get Started
+                        </Button>
+                    )}
+                />
+            </div>
         </div>
     );
 };
 
 const mapStateToProps = (state) => {
-    const { maxElderSpirits, maxNumHeroes } = state.cryptoChampions;
-    return {
-        maxElderSpirits,
-        maxNumHeroes
-    };
+    return {};
 };
 
 const mapDispatchToProps = (dispatch) => {
