5f26d9d81a8ba7bb1235bb620fb4812b2e7f606f
cds95 <cds95@uw.edu>
2021-03-30 23:40:27 -0700

    Update deploy local script

--- scripts/deploy_local.py
+++ scripts/deploy_local.py
@@ -2,25 +2,32 @@ from brownie import CryptoChampions, PriceWarsFactory, MockV3Aggregator, Minigam
 
 def main():
     INIT_BTC_PRICE = 50000
-    CURRENT_BTC_PRICE = 60000
-
     INIT_ETH_PRICE = 1600
-    CURRENT_ETH_PRICE = 1700
-
     INIT_LINK_PRICE = 27
-    CURRENT_LINK_PRICE = 30
-
-    PRICE_WARS = "PRICE_WARS"
+    INIT_DOT_PRICE = 34
+    INIT_BNB_PRICE = 310
+    INIT_UNI_PRICE = 29
+    INIT_ADA_PRICE = 1
 
     minigameFactoryRegistry = MinigameFactoryRegistry.deploy({ "from": accounts[0] })
     pwf = PriceWarsFactory.deploy({ "from": accounts[0] })
     cc = CryptoChampions.deploy(minigameFactoryRegistry.address, { "from": accounts[0] })
+
     btcV3Aggregator = MockV3Aggregator.deploy(18, INIT_BTC_PRICE, { "from": accounts[0] })
     ethV3Aggregator = MockV3Aggregator.deploy(18, INIT_ETH_PRICE, { "from": accounts[0] })
     linkV3Aggregator = MockV3Aggregator.deploy(18, INIT_LINK_PRICE, { "from": accounts[0] })
+    dotV3Aggregator = MockV3Aggregator.deploy(18, INIT_DOT_PRICE, { "from": accounts[0] })
+    bnbV3Aggregator = MockV3Aggregator.deploy(18, INIT_BNB_PRICE, { "from": accounts[0] })
+    uniV3Aggregator = MockV3Aggregator.deploy(18, INIT_UNI_PRICE, { "from": accounts[0] })
+    adaV3Aggregator = MockV3Aggregator.deploy(18, INIT_ADA_PRICE, { "from": accounts[0] })
 
+    PRICE_WARS = "PRICE_WARS"
     minigameFactoryRegistry.registerMinigame(PRICE_WARS, pwf.address)
 
     cc.createAffinity("BTC", btcV3Aggregator.address)
     cc.createAffinity("ETH", ethV3Aggregator.address)
     cc.createAffinity("LINK", linkV3Aggregator.address)
+    cc.createAffinity("DOT", dotV3Aggregator.address)
+    cc.createAffinity("BNB", bnbV3Aggregator.address)
+    cc.createAffinity("UNI", uniV3Aggregator.address)
+    cc.createAffinity("ADA", adaV3Aggregator.address)
