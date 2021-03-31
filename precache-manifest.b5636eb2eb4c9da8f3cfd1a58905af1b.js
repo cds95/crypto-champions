c114d5c303e51ca0b6bd42fec1a62545d9dbbb03
Rohhan <rohhan@gmail.com>
2021-03-31 00:30:51 -0700

    Updates

---
+++ precache-manifest.b5636eb2eb4c9da8f3cfd1a58905af1b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1000bf2ee62c8641a7fff1ddec78cf51",
+    "url": "/cryptochampz/index.html"
+  },
+  {
+    "revision": "e3fa8521a82a5dd8ddec",
+    "url": "/cryptochampz/static/css/main.7a4f2e63.chunk.css"
+  },
+  {
+    "revision": "180775369f4c26b562f1",
+    "url": "/cryptochampz/static/js/2.645abb11.chunk.js"
+  },
+  {
+    "revision": "5a66ed1c14d10646a61b2ba4b7983583",
+    "url": "/cryptochampz/static/js/2.645abb11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426c116dbee89db0b096",
+    "url": "/cryptochampz/static/js/3.5182f50d.chunk.js"
+  },
+  {
+    "revision": "0576523a805600aec22c",
+    "url": "/cryptochampz/static/js/4.dc62a90b.chunk.js"
+  },
+  {
+    "revision": "7452bdd4da55927fa937",
+    "url": "/cryptochampz/static/js/5.02934e98.chunk.js"
+  },
+  {
+    "revision": "199b2ae6676a1bc27665",
+    "url": "/cryptochampz/static/js/6.8ef86ceb.chunk.js"
+  },
+  {
+    "revision": "d7e27920616d4aa57545",
+    "url": "/cryptochampz/static/js/7.a1da4642.chunk.js"
+  },
+  {
+    "revision": "e3fa8521a82a5dd8ddec",
+    "url": "/cryptochampz/static/js/main.9fae5844.chunk.js"
+  },
+  {
+    "revision": "3ae822c4b0fbff33d089",
+    "url": "/cryptochampz/static/js/runtime-main.dda34501.js"
+  },
+  {
+    "revision": "15eb82a52024c90ea2dd9072c56ffd2c",
+    "url": "/cryptochampz/static/media/C01_human_male.15eb82a5.jpg"
+  },
+  {
+    "revision": "7671444a112403a9b95a517edb45b1a6",
+    "url": "/cryptochampz/static/media/C02_human_female.7671444a.jpg"
+  },
+  {
+    "revision": "394f41b754f7b5ee032dcfa2e1d465f1",
+    "url": "/cryptochampz/static/media/C03_elf_male.394f41b7.jpg"
+  },
+  {
+    "revision": "7d4a05455b595e853a20f49f21f7c0f7",
+    "url": "/cryptochampz/static/media/C04_elf_female.7d4a0545.jpg"
+  },
+  {
+    "revision": "c0af32999bfb149c649a959b335284dd",
+    "url": "/cryptochampz/static/media/C05_bear.c0af3299.jpg"
+  },
+  {
+    "revision": "65f9b667b2555df9bf8dc5d19ee51baa",
+    "url": "/cryptochampz/static/media/C06_bull.65f9b667.jpg"
+  },
+  {
+    "revision": "e5178eff216ed729abb3b11f6fddac38",
+    "url": "/cryptochampz/static/media/C07_frog.e5178eff.jpg"
+  },
+  {
+    "revision": "1479e2abd13746b668301f8573086375",
+    "url": "/cryptochampz/static/media/C08_robot.1479e2ab.jpg"
+  },
+  {
+    "revision": "0d2fb259ebea17cfa5cac1a22a54b3cc",
+    "url": "/cryptochampz/static/media/LeagueSpartan-Bold.0d2fb259.woff"
+  },
+  {
+    "revision": "e29e78646853571c0de071bb1229f03b",
+    "url": "/cryptochampz/static/media/LeagueSpartan-Regular.e29e7864.woff"
+  },
+  {
+    "revision": "fdc691e18ee3a40a4a3b3a50e6f637e4",
+    "url": "/cryptochampz/static/media/Prociono-Regular.fdc691e1.woff"
+  }
+]);
